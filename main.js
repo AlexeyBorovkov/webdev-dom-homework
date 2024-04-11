115d5af27eebf4ee6c5eb5d5110add2a568f51f6
AlexeyBorovkov <Borovkov86@inbox.ru>
2024-04-11 20:00:08 +0300

    2.17.1

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
-"use strict";
+import { getTodos } from "./api.js";
+import { renderComments } from "./renderComments.js";
+import { renderCommentForm } from "./renderForms.js";
 
-import { fetchAndRenderComments } from "./modulesForJs/api.js";
-import { delay } from "./modulesForJs/delay.js";
-import { initLikeButtonListeners } from "./modulesForJs/likeButton.js";
-import { removeValidation } from "./modulesForJs/removeValid.js";
-import { renderComments } from "./modulesForJs/renderComments.js";
-import { reply } from "./modulesForJs/reply.js";
-import { format } from "date-fns";
-
-// Получениe комментов с сервера
-export function getComments() {
-
-  fetchAndRenderComments().then((responseData) => {
-    const appComments = responseData.comments.map((comment) => {
-      return {
-        name: comment.author.name,
-        date: format(new Date(comment.date), 'yyyy-MM-dd hh.mm.ss'),
-        comment: comment.text,
-        likesCounter: comment.likes,
-        isLiked: comment.isLiked,
-      };
-    });
-    comments = appComments;
-    renderComments({ comments, initLikeButtonListeners, reply, removeValidation, delay });
-    // preLoadElement.classList.add('hide');
-  });
+export let user = null;
+export let comments = [];
+export function setUser(value) {
+  user = value;
+}
+export function renderApp() {
+  const container = document.querySelector('.container');
+  container.innerHTML = `
+  <ul class="comments" id = 'list'>
+  </ul>
+  <div class="form"></div>
+  <div class="loader"></div>`
+  getComments();
+  renderCommentForm();
 }
 
-getComments();
-// renderLogin({ getComments });
+export function getComments() {
+  const loader = document.querySelector(".loader");
+  const preloader = document.getElementById('preloader');
+    return getTodos().then((responseData) => {
+        const appComments = responseData.comments.map((comment) => {
+          return {
+            name: comment.author.name,
+            date: new Date(comment.date),
+            comment: comment.text,
+            likes: comment.likes,
+            isLiked: false,
+            id: comment.id,
+          }
+        });
+        comments = appComments;
+        renderComments({comments});
+        loader.textContent = '';
 
+        if (user) {
+        const addForm = document.querySelector(".add-form");
+          addForm.classList.remove("hidden");
+        }
+        preloader.classList.add('preloader-hidden');
+  });
+};
 
-
-//  Массив для комментов 
-let comments = [];
-
-// Кнопка для лайка 
-initLikeButtonListeners({ comments }, { renderComments });
-
-// Ответ по клику на коммент 
-reply({ comments });
+renderApp();
